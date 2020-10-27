b95ac5cf57df12eab6f9f4ec143cd199916bfd69
sheido <af8081@gmail.com>
2020-10-27 09:58:42 +0100

    AI first pass done with patrolling

--- RPG Core Combat Creator Course/Assets/Scripts/Control/AIController.cs
+++ RPG Core Combat Creator Course/Assets/Scripts/Control/AIController.cs
@@ -1,6 +1,7 @@
 ﻿using RPG.Combat;
 using RPG.Core;
 using RPG.Movement;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,15 +12,30 @@ namespace RPG.Control
     {
         private Fighter _fighter;
         private Health _health;
+        private Mover _mover;
         private GameObject _player;
+        private ActionScheduler _actionScheduler;
+        
+        private Vector3 _guardPosition;
+        private float _timeSinceLastSawPlayer = Mathf.Infinity;
+        private float _timeSinceArrivedAtWaypoint = Mathf.Infinity;
+        private int _currentWayPointIndex = 0;
 
         [SerializeField] private float _chaseDistance = 5f;
+        [SerializeField] private float _suspicionTime = 3f;
+        [SerializeField] private PatrolPath _patrolPath;
+        [SerializeField] private float _waypointTolerance = 1f;
+        [SerializeField] private float _waypointDwellTime = 1f; // waiting time
 
         void Start()
         {
             _fighter = GetComponent<Fighter>();
+            _mover = GetComponent<Mover>();
             _player = GameObject.FindGameObjectWithTag("Player");
             _health = GetComponent<Health>();
+            _actionScheduler = GetComponent<ActionScheduler>();
+
+            _guardPosition = transform.position;
         }
 
         private void Update()
@@ -28,18 +44,84 @@ namespace RPG.Control
 
             if (InAttackRangeOfPlayer() && _fighter.CanAttack(_player))
             {
-                _fighter.Attack(_player);
+                AttackBehavior();
+            }
+            else if (_timeSinceLastSawPlayer < _suspicionTime)
+            {
+                SuspicionBehavior();
             }
             else
             {
-                _fighter.Cancel();
+                PatrolBehavior();
             }
+
+            UpdateTimes();
+        }
+
+        private void UpdateTimes()
+        {
+            _timeSinceLastSawPlayer += Time.deltaTime;
+            _timeSinceArrivedAtWaypoint += Time.deltaTime;
+        }
+
+        private void PatrolBehavior()
+        {
+            Vector3 nextPosition = _guardPosition;
+
+            if (_patrolPath != null)
+            {
+                if (AtWayPoint())
+                {
+                    _timeSinceArrivedAtWaypoint = 0;
+                    CycleWayPoint();
+                }
+
+                nextPosition = GetCurrentWaypoint();
+            }
+
+            if (_timeSinceArrivedAtWaypoint > _waypointDwellTime)
+            {
+                _mover.StartMoveAction(nextPosition); // this cancels the previous action and move to next action
+            }
+        }
+        
+        private bool AtWayPoint()
+        {
+            float distanceToWayPoint = Vector3.Distance(transform.position, GetCurrentWaypoint());
+            return distanceToWayPoint < _waypointTolerance;
+        }
+
+        private void CycleWayPoint()
+        {
+            _currentWayPointIndex = _patrolPath.GetNextIndex(_currentWayPointIndex);
+        }
+
+        private Vector3 GetCurrentWaypoint()
+        {
+            return _patrolPath.GetWaypoint(_currentWayPointIndex);
+        }
+
+        private void SuspicionBehavior()
+        {
+            _actionScheduler.CancelCurrentAction();
+        }
+
+        private void AttackBehavior()
+        {
+            _timeSinceLastSawPlayer = 0;
+            _fighter.Attack(_player);
         }
 
         private bool InAttackRangeOfPlayer()
         {
             float distanceToPlayer = Vector3.Distance(_player.transform.position, transform.position);
             return distanceToPlayer < _chaseDistance;
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, _chaseDistance);
         }
     }
 }
