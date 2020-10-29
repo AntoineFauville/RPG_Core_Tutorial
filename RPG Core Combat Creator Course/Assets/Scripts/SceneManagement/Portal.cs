ad281ca761c877e63959a5826cf7de1ac545ab0b
sheido <af8081@gmail.com>
2020-10-29 19:56:06 +0100

    scene management partie 1

---
+++ RPG Core Combat Creator Course/Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField] private int _sceneToLoad = -1;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(_sceneToLoad);
+        }
+    }
+}
